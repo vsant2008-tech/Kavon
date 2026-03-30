95a6c954bd17344d24bc39956ee33131cc8bf91c
vsant2008-tech <vsant2008@gmail.com>
2026-03-30 19:17:33 -0400

    Add console log for Supabase URL

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+console.log('Supabase URL:', import.meta.env.VITE_SUPABASE_URL);
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
